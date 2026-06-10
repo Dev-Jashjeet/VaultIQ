dedc72f53ec39ec54a67ee21063084110b24eb06
Jashjeet Singh <jashjeet99@gmail.com>
2026-06-10 19:36:48 +0530

    Add getData function to fetch content from Gemini API

---
+++ geminiApi.js
@@ -0,0 +1,25 @@
+export default async function getData(prompt) {
+    const response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-goog-api-key": "AQ.Ab8RN6LJkiawRrQfI-43cEJUGCT5amV22TBgOVnCps8T02Z9SA"
+        },
+        body: JSON.stringify({
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": prompt
+                        }
+                    ]
+                }
+            ]
+        })
+    });
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error("Some error in fetching API");
+    }
+    return data.candidates[0].content.parts[0].text;
+};
